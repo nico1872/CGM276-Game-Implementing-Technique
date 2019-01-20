8dd338100e90988f2f817f67a844f3e8833e315c
nico1872 <46856031+nico1872@users.noreply.github.com>
2019-01-20 11:54:27 +0700

    UDP

---
+++ 01/UDPClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String server = "10.1.116.239";
+
+            int servPort=5000;
+            String message="Nicolas 5902828";
+            byte[] sendPacket= Encoding.ASCII.GetBytes(message);
+            UdpClient client = new UdpClient();
+            
+            try{
+                    client.Send(sendPacket,sendPacket.Length,server,servPort);
+
+                   
+                }catch(SocketException e){
+                        Console.WriteLine(e.ErrorCode+" "+e.Message);
+                }
+        }
+    }
+}
---
+++ 01/UDPServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int servPort=5000;
+             UdpClient client=null;
+             client=new UdpClient(servPort);
+IPEndPoint remoteIPEndPoint=new IPEndPoint(IPAddress.Any,0);
+
+
+             for(;;){   //คือให้server รันอยู่ตลอดเวลา
+                try{
+                    byte[] byteBuffer = client.Receive(ref remoteIPEndPoint);
+
+                    String recvMessage = Encoding.ASCII.GetString(byteBuffer,0,byteBuffer.Length);
+                    Console.WriteLine(recvMessage);
+                }catch(SocketException e){
+                        Console.WriteLine(e.ErrorCode+" "+e.Message);
+                }
+             }
+        }
+    }
+}
